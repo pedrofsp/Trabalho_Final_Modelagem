41367e5a81dd9451e97c046412e5f9466b74d015
Pedro Sampaio <pedrohenriquefsp90@gmail.com>
2021-04-16 09:11:16 -0300

    Implementacao domain parte 2 (bug)

--- App-Trabalho/src/app/app-routing.module.ts
+++ App-Trabalho/src/app/app-routing.module.ts
@@ -4,23 +4,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
-    path: 'weather',
-    loadChildren: () => import('./weather/weather.module').then( m => m.WeatherPageModule)
+    path: 'weather/:id',
+    loadChildren: () =>
+      import('./weather/weather.module').then((m) => m.WeatherPageModule),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- App-Trabalho/src/app/app.module.ts
+++ App-Trabalho/src/app/app.module.ts
@@ -8,9 +8,18 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SearchCityService } from 'src/domain/services/search-city-service';
 import { FakeCityRepository } from 'src/data/fake/fake-city-repository';
+import { LoadWeatherService } from 'src/domain/services/load-weather.service';
+import { FakeWeatherRepository } from 'src/data/fake/fake-weather-repository';
 
 const createSearchCityService = () => {
   return new SearchCityService(new FakeCityRepository());
+};
+
+const createLoadWeatherService = () => {
+  return new LoadWeatherService(
+    new FakeCityRepository(),
+    new FakeWeatherRepository()
+  );
 };
 
 @NgModule({
@@ -20,6 +29,7 @@ const createSearchCityService = () => {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: SearchCityService, useFactory: createSearchCityService },
+    { provide: LoadWeatherService, useFactory: createLoadWeatherService },
   ],
   bootstrap: [AppComponent],
 })
